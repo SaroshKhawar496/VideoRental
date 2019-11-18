9283f6130c3d0268669a885b3c0df5411971d94c
SaroshKhawar496 <sarosh.496@gmail.com>
2019-11-18 14:23:09 -0500

    Add project files.

---
+++ VideoRental/App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace VideoRental
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //enabling attribute routing
+            routes.MapMvcAttributeRoutes();
+
+            //need to define most specific to generic routes, top to bottom
+
+            //genric way of routing ie old way
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2015 | 2016", month = @"\d{2}" }
+            //    );
+            routes.MapRoute(
+                "CustomersIndex",
+                "customers"
+                )
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
